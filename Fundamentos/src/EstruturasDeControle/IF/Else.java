0fd3ee1ebf0dd85f9b6e630747a2ccb75411acfa
MatheusRocco <matheusroccogg@gmail.com>
2026-01-12 16:00:07 -0300

    Added IF/IFelse

---
+++ Fundamentos/src/EstruturasDeControle/IF/Else.java
@@ -0,0 +1,18 @@
+package EstruturasDeControle.IF;
+
+import java.util.Scanner;
+
+public class Else {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Informe um numero");
+        int numero = entrada.nextInt();
+
+        if(numero % 2 == 0){
+            System.out.println("Seu numero é par");
+        }else{
+            System.out.println("Seu numero é impar");
+        }
+    }
+}
---
+++ Fundamentos/src/EstruturasDeControle/IF/ElseIF.java
@@ -0,0 +1,26 @@
+package EstruturasDeControle.IF;
+
+import java.util.Scanner;
+
+public class ElseIF {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Informe uma nota de 0 a 10");
+        double numero = entrada.nextDouble();
+
+        if(numero >10 || numero <=0){
+            System.out.println("Nota invalida");
+        } else {
+            System.out.println("Nota valida");
+
+        }if (numero >=8){
+            System.out.println("Numero A");
+        } else if (numero>=4) {
+            System.out.println("Numero B");
+        } else if (numero>=2) {
+            System.out.println("Numero c" );
+
+        }
+    }
+}
