c678277933241fe2167e385aff74768b5ae716aa
MatheusRocco <matheusroccogg@gmail.com>
2026-02-25 16:40:50 -0300

    StreamAPI and challenge Map

---
+++ Fundamentos/src/StreamAPIs/DESAFIOMAP1/DESAFIOMAP1.java
@@ -0,0 +1,18 @@
+package StreamAPIs.DESAFIOMAP1;
+
+import java.util.*;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
+
+public class DESAFIOMAP1 {
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(1,2,3,4,5,6,7,8,9);
+        nums.stream().map(n->Integer.toBinaryString(n);
+        nums.stream().map(n->UnaryOperator<String> StringBuilder.reverse());
+        //nums.stream().map(n-> UnaryOperator.)
+
+
+    }
+
+
+}
